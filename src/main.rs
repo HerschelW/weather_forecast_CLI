53852dcfe10624179c12e70a12527c35ed5c52d9
HerschelW <herschelw@gmail.com>
2023-05-30 10:57:08 -0500

    updating dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+// import clap parser
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "wet")]
+#[command(about = "Weather in your terminal", long_about = None)]
+
+struct Args {
+    days: u8,
+}
+
 //  Create a CLI
 
 // Make HTTP request and parse output
